f6164259ba7a8918fe7f40671135e820b8b54e55
AliFeizi94 <ali.feizi94@yahoo.com>
2023-11-22 19:36:07 +0330

    end of writing  python_Advance_Practice-01

--- Python_Advance/python_Advance_Practice-01.py
+++ Python_Advance/python_Advance_Practice-01.py
@@ -1,5 +1,6 @@
 
 
+##################  Fanction for Identifying and listing the  divisors of the input number ############
 def MyFunction(x):
     Divisor=list()
     Row=1
@@ -7,9 +8,14 @@ def MyFunction(x):
         if x%Row==0:
             Divisor.append(Row)
         Row +=1
-    print(Divisor)
+    #print(Divisor)
     return Divisor
+##########################################################################################################
 
+
+
+
+#################  Fanction for Identifying and listing the prime divisors of the input number ###########
 def  prime_number(x):
     N=2
     while N!=x+1 :
@@ -19,30 +25,60 @@ def  prime_number(x):
         elif x==N or x==1:
             Answer="True" # aval ast
         N +=1
-    print(Answer)
+    #print(x," Aval " , Answer)
     return Answer   
+##########################################################################################################
 
+
+
+
+################## input numbers ########################################################################
 n=0
 Divisor_Dectionery=dict()
-while n!=3 :
+while n!=10 :
+
    x=int(input())
    Divisor_Dectionery[x]=MyFunction(x)
    n +=1
-print(Divisor_Dectionery)
+#print(Divisor_Dectionery)
+#########################################################################################################
 
 
+
+
+
+################### loop for add only prime divisors in to divisors list of input numbers ###############
 for Row in Divisor_Dectionery:
     List=Divisor_Dectionery[Row]
+    #Primer_one_List=list()
     for Row in List :
-        if Row!=1 and prime_number(Row)=="false" :
-            List.remove(Row)
+        if Row==1 :
+            Primer_one_List=list()
+        elif prime_number(Row)=="True" :
+            Primer_one_List.append(Row)
+        #print(Primer_one_List)
+    Divisor_Dectionery[Row]=Primer_one_List
+    #print(Divisor_Dectionery)
+#print(Divisor_Dectionery)
+#########################################################################################################
 
-        
-print(List)
 
 
-#
 
-#sorted_Mydictionary =dict(sorted(Mydictionary.items(), key=lambda x: (x[1],x[0]) , reverse=True))
 
+#################### loop for counter of prime divisors of input numbers ################################
+for key in Divisor_Dectionery:
+    Divisor_Dectionery[key]=len(Divisor_Dectionery[key])
+########################################################################################################
+
+
+
+
+
+######### loop for Sort the dictionery of input numbers and prime divisors of input number an print it ##
+sorted_Mydictionary =dict(sorted(Divisor_Dectionery.items(), key=lambda x: (x[1],x[0]) , reverse=True))
+for key in sorted_Mydictionary :
+    print(key ,sorted_Mydictionary[key])
+    break
+##########################################################################################################
    
